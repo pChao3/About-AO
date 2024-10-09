d56936d3ccdf2b8a86519204e746f25a23d87979
吕品超 <lvpinchao@xingheng.ai>
2024-10-09 18:12:40 +0800

    delete useless comp

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,4 @@
 import Image from 'next/image';
-import { Inter } from '@next/font/google';
-import styles from '@/styles/Home.module.css';
-import Hero from '@/components/home/Hero';
-import Features from '@/components/home/Features';
-import Testimonials from '@/components/home/Testimonials';
-import GenerationStep from '@/components/home/GenerationStep';
-import FeatureSection from '@/components/home/FeatureSection';
-import PageMetaTags from '@/containers/PageMetaTags';
-import CTA2 from '@/components/home/CTA2';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
   return (
     <>
